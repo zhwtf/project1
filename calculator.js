e1f63df2e789b8ed9ab63816c8742e8b70728810
郑昊 <zhenghao@zhenghaodeMacBook-Air.local>
2016-09-25 23:32:48 -0700

    Add weighted function

--- calculator.js
+++ calculator.js
@@ -37,8 +37,27 @@ function mean(){
       count++;
     }
   }
-  var mean = sum / count * 100;
+  var mean = (sum / count) * 100;
   mean = Math.round(mean*100) / 100;
   document.getElementById('show').innerHTML = "Mean: " + mean + "%";
+}
+
+function weighted(){
+  var sum = 0;
+  var weightsum = 0;
+  for (var i = 1; i<=RowCount; i++){
+    var weight = document.getElementById("W"+i).value;
+    var numerator = document.getElementById("A"+i+"_num").value;
+    var denominator = document.getElementById("A"+i+"_den").value;
+    var percent = numerator / denominator;
+    //percent = Math.round(percent * 100) / 100;
+    if (isFinite(percent)==true && weight>0) {
+      sum = sum + percent*weight;
+      weightsum = weightsum + parseInt(weight);
+    }
+  }
+  var output = (sum / weightsum) * 100;
+  output = Math.round(output*100) / 100;
+  document.getElementById('show').innerHTML = "Weighted: " + output + "%";
 }
 
